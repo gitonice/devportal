6090360f8dca5916d756c70b4d688a6d10b0484b
Pétur Sigurðsson <pes@annata.net>
2018-06-25 14:30:16 +0800

    First committ
    Fyrsta committ

---
+++ AnnataProceedures/Annata Guidelines/1.0/en-us/toc.js
@@ -0,0 +1,511 @@
+﻿var tocData = [
+{
+
+  text: "Customer Projects", 
+
+  href: "BpmBusinessProcess_5637144865.html"
+,
+  nodes: [
+{
+
+  text: "Sales", 
+
+  href: "BpmBusinessProcess_5637144887.html"
+,
+  nodes: [
+{
+
+  text: "Prepare demo environment", 
+
+  href: "BpmBusinessProcess_5637144899.html"
+
+},
+{
+
+  text: "Demonstrate solution", 
+
+  href: "BpmBusinessProcess_5637144898.html"
+
+}
+  ]
+
+},
+{
+
+  text: "Delivery", 
+
+  href: "BpmBusinessProcess_5637144889.html"
+,
+  nodes: [
+{
+
+  text: "Deliver solution release", 
+
+  href: "BpmBusinessProcess_5637144894.html"
+
+},
+{
+
+  text: "Deliver solution license", 
+
+  href: "BpmBusinessProcess_5637144897.html"
+
+},
+{
+
+  text: "Deliver solution update", 
+
+  href: "BpmBusinessProcess_5637144895.html"
+
+},
+{
+
+  text: "Deliver solution hotfix", 
+
+  href: "BpmBusinessProcess_5637144896.html"
+
+}
+  ]
+
+},
+{
+
+  text: "Implementation", 
+
+  href: "BpmBusinessProcess_5637144888.html"
+
+},
+{
+
+  text: "Training", 
+
+  href: "BpmBusinessProcess_5637144890.html"
+,
+  nodes: [
+{
+
+  text: "Training intake and preparation", 
+
+  href: "BpmBusinessProcess_5637144891.html"
+
+},
+{
+
+  text: "Conduct training session", 
+
+  href: "BpmBusinessProcess_5637144892.html"
+
+},
+{
+
+  text: "Training follow-up", 
+
+  href: "BpmBusinessProcess_5637144893.html"
+
+}
+  ]
+
+},
+{
+
+  text: "Support", 
+
+  href: "BpmBusinessProcess_5637144879.html"
+,
+  nodes: [
+{
+
+  text: "Handle support case", 
+
+  href: "BpmBusinessProcess_5637144880.html"
+
+},
+{
+
+  text: "Resolve issue", 
+
+  href: "BpmBusinessProcess_5637144885.html"
+
+},
+{
+
+  text: "Create FAQ", 
+
+  href: "BpmBusinessProcess_5637144886.html"
+
+}
+  ]
+
+}
+  ]
+
+},
+{
+
+  text: "Product Development", 
+
+  href: "BpmBusinessProcess_5637144940.html"
+,
+  nodes: [
+{
+
+  text: "Roadmap", 
+
+  href: "BpmBusinessProcess_5637144941.html"
+,
+  nodes: [
+{
+
+  text: "Suggest ideas", 
+
+  href: "BpmBusinessProcess_5637144942.html"
+
+},
+{
+
+  text: "Specify features", 
+
+  href: "BpmBusinessProcess_5637144943.html"
+
+},
+{
+
+  text: "Manage roadmap", 
+
+  href: "BpmBusinessProcess_5637144944.html"
+
+},
+{
+
+  text: "Communicate roadmap", 
+
+  href: "BpmBusinessProcess_5637144945.html"
+
+}
+  ]
+
+},
+{
+
+  text: "Development", 
+
+  href: "BpmBusinessProcess_5637144946.html"
+,
+  nodes: [
+{
+
+  text: "Prepare development environment", 
+
+  href: "BpmBusinessProcess_5637144947.html"
+,
+  nodes: [
+{
+
+  text: "Prepare development environment for D365", 
+
+  href: "BpmBusinessProcess_5637144990.html"
+
+},
+{
+
+  text: "Prepare development environment for D365FO", 
+
+  href: "BpmBusinessProcess_5637144991.html"
+
+}
+  ]
+
+},
+{
+
+  text: "Manage versions", 
+
+  href: "BpmBusinessProcess_5637144948.html"
+
+},
+{
+
+  text: "Prepare software deliverables", 
+
+  href: "BpmBusinessProcess_5637144949.html"
+,
+  nodes: [
+{
+
+  text: "Prepare software deliverables for D365", 
+
+  href: "BpmBusinessProcess_5637144992.html"
+
+},
+{
+
+  text: "Prepare software deliverables for D365FO", 
+
+  href: "BpmBusinessProcess_5637144993.html"
+
+},
+{
+
+  text: "Prepare software deliverables for AX2012", 
+
+  href: "BpmBusinessProcess_5637144994.html"
+
+}
+  ]
+
+},
+{
+
+  text: "Prepare documentation", 
+
+  href: "BpmBusinessProcess_5637144950.html"
+,
+  nodes: [
+{
+
+  text: "Prepare documentation for D365", 
+
+  href: "BpmBusinessProcess_5637144995.html"
+
+},
+{
+
+  text: "Prepare documentation for D365FO", 
+
+  href: "BpmBusinessProcess_5637144996.html"
+
+},
+{
+
+  text: "Prepare documentation for AX2012", 
+
+  href: "BpmBusinessProcess_5637144997.html"
+
+}
+  ]
+
+}
+  ]
+
+},
+{
+
+  text: "Build", 
+
+  href: "BpmBusinessProcess_5637144951.html"
+
+},
+{
+
+  text: "Testing", 
+
+  href: "BpmBusinessProcess_5637144952.html"
+,
+  nodes: [
+{
+
+  text: "Design tests", 
+
+  href: "BpmBusinessProcess_5637144953.html"
+
+},
+{
+
+  text: "Run exploratory tests", 
+
+  href: "BpmBusinessProcess_5637144954.html"
+
+},
+{
+
+  text: "Develop automated tests", 
+
+  href: "BpmBusinessProcess_5637144955.html"
+
+},
+{
+
+  text: "Run automated tests", 
+
+  href: "BpmBusinessProcess_5637144956.html"
+
+},
+{
+
+  text: "Report test results", 
+
+  href: "BpmBusinessProcess_5637144957.html"
+
+}
+  ]
+
+},
+{
+
+  text: "Release", 
+
+  href: "BpmBusinessProcess_5637144958.html"
+,
+  nodes: [
+{
+
+  text: "Verify solution readiness", 
+
+  href: "BpmBusinessProcess_5637144959.html"
+,
+  nodes: [
+{
+
+  text: "Verify solution readiness for D365", 
+
+  href: "BpmBusinessProcess_5637144998.html"
+
+},
+{
+
+  text: "Verify solution readiness for D365FO", 
+
+  href: "BpmBusinessProcess_5637144999.html"
+
+}
+  ]
+
+},
+{
+
+  text: "Release solution", 
+
+  href: "BpmBusinessProcess_5637144960.html"
+,
+  nodes: [
+{
+
+  text: "Release solution for D365", 
+
+  href: "BpmBusinessProcess_5637145000.html"
+
+},
+{
+
+  text: "Release solution for D365FO", 
+
+  href: "BpmBusinessProcess_5637145001.html"
+
+}
+  ]
+
+},
+{
+
+  text: "Release update", 
+
+  href: "BpmBusinessProcess_5637144961.html"
+,
+  nodes: [
+{
+
+  text: "Release update for D365", 
+
+  href: "BpmBusinessProcess_5637145002.html"
+
+},
+{
+
+  text: "Release update for D365FO", 
+
+  href: "BpmBusinessProcess_5637145003.html"
+
+},
+{
+
+  text: "Release update for AX2012", 
+
+  href: "BpmBusinessProcess_5637145004.html"
+
+}
+  ]
+
+},
+{
+
+  text: "Release hotfix", 
+
+  href: "BpmBusinessProcess_5637144962.html"
+,
+  nodes: [
+{
+
+  text: "Release hotfix for D365", 
+
+  href: "BpmBusinessProcess_5637145005.html"
+
+},
+{
+
+  text: "Release hotfix for D365FO", 
+
+  href: "BpmBusinessProcess_5637145006.html"
+
+}
+  ]
+
+},
+{
+
+  text: "Launch solution release", 
+
+  href: "BpmBusinessProcess_5637144963.html"
+
+},
+{
+
+  text: "AppSource curation", 
+
+  href: "BpmBusinessProcess_5637144964.html"
+
+}
+  ]
+
+}
+  ]
+
+},
+{
+
+  text: "Supporting Processes", 
+
+  href: "BpmBusinessProcess_5637144881.html"
+,
+  nodes: [
+{
+
+  text: "Finance", 
+
+  href: "BpmBusinessProcess_5637144882.html"
+
+},
+{
+
+  text: "HR", 
+
+  href: "BpmBusinessProcess_5637144883.html"
+
+},
+{
+
+  text: "IT", 
+
+  href: "BpmBusinessProcess_5637144884.html"
+
+},
+{
+
+  text: "Facilities", 
+
+  href: "BpmBusinessProcess_5637144914.html"
+
+}
+  ]
+
+}
+];
